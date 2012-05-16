de087ab126049112771cb9144988e08bc7396b01
Pravat <pravat_maskey@hotmail.com>
2012-05-16 22:13:43 +0545

    Project initialize

---
+++ wijzer/DataViewController.h
@@ -0,0 +1,16 @@
+//
+//  DataViewController.h
+//  wijzer
+//
+//  Created by Pravat Maskey on 5/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *dataLabel;
+@property (strong, nonatomic) id dataObject;
+
+@end
---
+++ wijzer/RootViewController.h
@@ -0,0 +1,15 @@
+//
+//  RootViewController.h
+//  wijzer
+//
+//  Created by Pravat Maskey on 5/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController <UIPageViewControllerDelegate>
+
+@property (strong, nonatomic) UIPageViewController *pageViewController;
+
+@end
